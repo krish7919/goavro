66043036efa2cc0f16f8093d44bc4a9d25738874
Krish <kriiyer@cisco.com>
2018-07-02 15:52:11 +0100

    Modify the json format during conversion

--- union.go
+++ union.go
@@ -40,7 +40,9 @@ func Union(name string, datum interface{}) interface{} {
 	if datum == nil && name == "null" {
 		return nil
 	}
-	return map[string]interface{}{name: datum}
+	//return map[string]interface{}{name: datum}
+	// Return the datum directly instead of encapsulating it in the data type
+	return datum
 }
 
 func buildCodecForTypeDescribedBySlice(st map[string]*Codec, enclosingNamespace string, schemaArray []interface{}) (*Codec, error) {
